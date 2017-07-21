a64b18a3b29bf85105e263f5ab789ce5dd7a97f5
Nurbolat <nurbolatsnk@gmail.com>
2017-07-21 19:58:59 +0600

    add some text to view in courses app

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Course
 # Create your views here.
+def course_list(request):
+    courses=Course.objects.all()
+    output = ', '.join(courses)
+    return HttpResponse(output)
